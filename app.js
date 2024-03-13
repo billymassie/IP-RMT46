9c290b6c6106570b22f405273896fe8ac78904b2
billy.massie <billybois68@gmail.com>
2024-03-13 21:53:29 +0700

    feat: setup app and basic route

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(require('./routes'));
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const router = require('express').Router();
+
+router.use('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = router;
