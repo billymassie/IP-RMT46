c6424daea88ff981e45782cbb0d07c671ca3fc3d
billy.massie <billybois68@gmail.com>
2024-03-15 15:37:56 +0700

    feat: move folder

--- helpers/bcrypt.js
+++
@@ -1,7 +0,0 @@
-const { hashSync, compareSync } = require('bcryptjs');
-
-module.exports = {
-  hashPassword: (password) => hashSync(password, 8),
-  comparePassword: (password, db_password) =>
-    compareSync(password, db_password),
-};
